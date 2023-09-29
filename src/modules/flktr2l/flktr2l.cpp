158e927aef01ebfb808a49a6f11465faaa886b1e
Henrik Battke <henrik.battke@yahoo.de>
2023-09-29 15:55:28 +0200

    UART communication to tool driver

--- src/modules/flktr2l/flktr2l.cpp
+++ src/modules/flktr2l/flktr2l.cpp
@@ -12,32 +12,32 @@
 
 #include <uORB/topics/parameter_update.h>
 #include <uORB/topics/vehicle_status.h>
-#include <uORB/topics/adc_report.h>
+//#include <uORB/topics/adc_report.h>
 
 #include <parameters/param.h>
 
 int Flktr2L::print_status() {
-  PX4_INFO("Running, failsafe status : %d", _failsafe);
+  PX4_INFO("Running, failsafe status : %d, scale : %lu", _failsafe, _package._scale);
 
-  return 0;
+  return PX4_OK;
 }
 
 int Flktr2L::custom_command(int argc, char *argv[]) {
-    if (!is_running()) {
-      print_usage("not running");
-      return 1;
-    }
 
-    // additional custom commands can be handled like this:
-    if (!strcmp(argv[0], "adc")) {
+  if (!is_running()) {
+    print_usage("not running");
+    return PX4_ERROR;
+  }
 
-    get_instance()->adc();
-    return 0;
-    }
+  // additional custom commands can be handled like this:
+  /*
+  if (!strcmp(argv[0], "xyz")) {
+    get_instance()->xyz();
+    return PX4_OK;
+    }*/
 
   return print_usage("unknown command");
 }
-
 
 int Flktr2L::task_spawn(int argc, char *argv[]) {
   _task_id = px4_task_spawn_cmd("flktr2l",
@@ -52,7 +52,7 @@ int Flktr2L::task_spawn(int argc, char *argv[]) {
     return -errno;
   }
 
-  return 0;
+  return PX4_OK;
 }
 
 Flktr2L *Flktr2L::instantiate(int argc, char *argv[]) {
@@ -61,14 +61,36 @@ Flktr2L *Flktr2L::instantiate(int argc, char *argv[]) {
   int myoptind = 1;
   int ch;
   const char *myoptarg = nullptr;
+  char port[20] = "/dev/ttyS1";
+  speed_t speed = B115200;
 
   // parse CLI arguments
-  while ((ch = px4_getopt(argc, argv, "", &myoptind, &myoptarg)) != EOF) {
+  while ((ch = px4_getopt(argc, argv, "d:b:", &myoptind, &myoptarg)) != EOF) {
     switch (ch) {
     case '?':
       error_flag = true;
       break;
+
+    case 'd':      
+      strncpy(port, myoptarg, sizeof(port));
+      port[sizeof(port)-1] = 0x0;
+      break;
       
+    case 'b':
+      switch (atoi(myoptarg)) {
+	case 9600:   speed = B9600;   break;
+	case 19200:  speed = B19200;  break;
+	case 38400:  speed = B38400;  break;
+	case 57600:  speed = B57600;  break;
+	case 115200: speed = B115200; break;
+	case 230400: speed = B230400; break;
+	  default:
+	    PX4_WARN("unknown baud rate");
+	    error_flag = true;
+	}
+
+      break;
+
     default:
       PX4_WARN("unrecognized flag");
       error_flag = true;
@@ -80,7 +102,7 @@ Flktr2L *Flktr2L::instantiate(int argc, char *argv[]) {
     return nullptr;
   }
 
-  Flktr2L *instance = new Flktr2L();
+  Flktr2L *instance = new Flktr2L(port, speed);
 
   if (instance == nullptr) {
     PX4_ERR("alloc failed");
@@ -89,27 +111,119 @@ Flktr2L *Flktr2L::instantiate(int argc, char *argv[]) {
   return instance;
 }
 
-Flktr2L::Flktr2L()
+Flktr2L::Flktr2L(const char *port, const speed_t speed)
   : ModuleParams(nullptr) {
+  strncpy(_port, port, sizeof(_port));
+  _port[sizeof(_port)-1]=0x0;
+  _speed = speed;
+  _timestamp_last_sample = hrt_absolute_time(); //reset time counter
+}
+
+int Flktr2L::open_serial_port(const char *port, const speed_t speed) {  
+  // File descriptor initialized?
+  if (_file_descriptor > 0) {
+    PX4_DEBUG("serial port already open");
+    return PX4_OK;
+  }
+
+  // Configure port flags for read/write, non-controlling, non-blocking.
+  int flags = (O_RDWR | O_NOCTTY | O_NONBLOCK);
+
+  // Open the serial port.
+  _file_descriptor = ::open(port, flags);
+
+  if (_file_descriptor < 0) {
+    PX4_ERR("open failed (%i)", errno);
+    return PX4_ERROR;
+  }
+
+  if (!isatty(_file_descriptor)) {
+    PX4_WARN("not a serial device");
+    return PX4_ERROR;
+  }
+
+  termios uart_config{};
+
+  // Store the current port configuration. attributes.
+  if (tcgetattr(_file_descriptor, &uart_config)) {
+    PX4_ERR("Unable to get termios from %s.", port);
+    ::close(_file_descriptor);
+    _file_descriptor = -1;
+    return PX4_ERROR;
+  }
+
+  // Clear: data bit size, two stop bits, parity, hardware flow control.
+  uart_config.c_cflag &= ~(CSIZE | CSTOPB | PARENB | CRTSCTS);
+
+  // Set: 8 data bits, enable receiver, ignore modem status lines.
+  uart_config.c_cflag |= (CS8 | CREAD | CLOCAL);
+
+  // Clear: echo, echo new line, canonical input and extended input.
+  uart_config.c_lflag &= (ECHO | ECHONL | ICANON | IEXTEN);
+
+  // Clear ONLCR flag (which appends a CR for every LF).
+  uart_config.c_oflag &= ~ONLCR;
+
+  // Set the input baud rate in the uart_config struct.
+  int termios_state = cfsetispeed(&uart_config, speed);
+
+  if (termios_state < 0) {
+    PX4_ERR("CFG: %d ISPD", termios_state);
+    ::close(_file_descriptor);
+    return PX4_ERROR;
+  }
+
+  // Set the output baud rate in the uart_config struct.
+  termios_state = cfsetospeed(&uart_config, speed);
+
+  if (termios_state < 0) {
+    PX4_ERR("CFG: %d OSPD", termios_state);
+    ::close(_file_descriptor);
+    return PX4_ERROR;
+  }
+
+  // Apply the modified port attributes.
+  termios_state = tcsetattr(_file_descriptor, TCSANOW, &uart_config);
+
+  if (termios_state < 0) {
+    PX4_ERR("baud %d ATTR", termios_state);
+    ::close(_file_descriptor);
+    return PX4_ERROR;
+  }
+
+  // Flush the hardware buffers.
+  tcflush(_file_descriptor, TCIOFLUSH);
+
+  //PX4_INFO("successfully opened UART port %s", port);
+
+  return PX4_OK;
+}
+
+void Flktr2L::_shiftAndAdd(uint8_t oneByte) {
+  uint8_t *pkg = (uint8_t*)&_package;
+  for(unsigned int i=1; i<sizeof(_package); ++i) {
+    pkg[i-1] = pkg[i];
+  }
+  pkg[sizeof(_package)-1] = oneByte;
 }
 
 void Flktr2L::run() {
   struct vehicle_status_s vehicle_status;
-  struct adc_report_s adc_report;
+  //struct adc_report_s adc_report;
 
   int vehicle_status_sub = orb_subscribe(ORB_ID(vehicle_status));
-  int adc_report_sub = orb_subscribe(ORB_ID(adc_report));
+  //int adc_report_sub = orb_subscribe(ORB_ID(adc_report));
 
-  px4_pollfd_struct_t fds[2];
+  px4_pollfd_struct_t fds[1];
   fds[0].fd = vehicle_status_sub;
   fds[0].events = POLLIN;
-  fds[1].fd = adc_report_sub;
-  fds[1].events = POLLIN;
+  //fds[1].fd = adc_report_sub;
+  //fds[1].events = POLLIN;
 
   // initialize parameters
   parameters_update(true);
 
-  hrt_abstime _timestamp_last_adc_report = hrt_absolute_time();
+  //hrt_abstime _timestamp_last_adc_report = hrt_absolute_time();
   
   while (!should_exit()) {
     // wait for up to 1000ms for data
@@ -144,6 +258,7 @@ void Flktr2L::run() {
 	  }
 	}
       }
+      /*
       if (fds[1].revents & POLLIN) {
 	if (hrt_elapsed_time(&_timestamp_last_adc_report) > 900000) {
 	  _timestamp_last_adc_report = hrt_absolute_time(); //reset time counter
@@ -156,22 +271,61 @@ void Flktr2L::run() {
 	  _tool_status_pub.publish(_msg);
 	}
       }
+      */
 
       parameters_update();
     }
+
+    //UART communication
+    open_serial_port(_port, _speed);
+    bool res = false;
+    int bytes_read = 0;
+    while((bytes_read = ::read(_file_descriptor, &_buffer[0], sizeof(_buffer))) >0) {	
+      for(int i=0; i<bytes_read; i++) {
+	_shiftAndAdd(_buffer[i]);
+	if(_package._header[0] == sReferenceHeader[0] && _package._header[1] == sReferenceHeader[1]) {
+	  // we found a valid frame
+	  res = true;
+	  _msg.id=0;
+	  _msg.timestamp = hrt_absolute_time();
+	  _msg.data_1 = _package._scale;
+	  _msg.data_2 = 0;
+	  //PX4_INFO("%lu", _msg.data_1);
+	}
+      }
+    }
+
+    if(res) {
+      _tool_status_pub.publish(_msg); //last value only
+      _timestamp_last_sample = hrt_absolute_time(); //remember when we read the last valid package
+    }
+    else {
+      //when did we read the last valid package
+      if (hrt_elapsed_time(&_timestamp_last_sample) > 10000000) {
+	_timestamp_last_sample = hrt_absolute_time(); //reset time counter
+	//let's try to reopen the port
+	::close(_file_descriptor);
+	_file_descriptor = -1;
+	open_serial_port(_port, _speed);
+      }
+      }
+    
     px4_usleep(100000);
   }
 
   orb_unsubscribe(vehicle_status_sub);
-  orb_unsubscribe(adc_report_sub);
+  //orb_unsubscribe(adc_report_sub);
+  close(_file_descriptor);
 }
 
+/*
 void Flktr2L::adc() {
   PX4_INFO("ADC: %ld %ld",
 	_msg.data_1,
 	_msg.data_2
   );
 }
+*/
 
 void Flktr2L::parameters_update(bool force) {
   // check for parameter updates
@@ -192,7 +346,7 @@ int Flktr2L::print_usage(const char *reason) {
   
   PRINT_MODULE_DESCRIPTION(R"DESCR_STR(
 ### Description
-The module flktr2l supports start/stop/status/adc functionality and runs in the background.
+The module flktr2l supports start/stop/status functionality and runs in the background.
 
 ### Implementation
 The module flktr2l is listening to the vehicle and tool status. If a failsafe status is triggered, the actuators will be turned off. Depending on the tool it collects tool depending information.
@@ -204,10 +358,13 @@ $ flktr2l start
 )DESCR_STR");
 
   PRINT_MODULE_USAGE_NAME("flktr2l", "system");
-  PRINT_MODULE_USAGE_COMMAND("start");
+  PRINT_MODULE_USAGE_COMMAND_DESCR("start", "Start module");
+  PRINT_MODULE_USAGE_PARAM_STRING('d', "/dev/ttyS1", "/dev/ttyS[0-9]", "Serial device", false);
+  PRINT_MODULE_USAGE_PARAM_STRING('b', "115200", "9600, 19200, 38400, 57600, 115200, 230400", "Baud rate", false);
+
   PRINT_MODULE_USAGE_DEFAULT_COMMANDS();
 
-  return 0;
+  return PX4_OK;
 }
 
 int flktr2l_main(int argc, char *argv[]) {
--- src/modules/flktr2l/flktr2l.h
+++ src/modules/flktr2l/flktr2l.h
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include <termios.h>
+
 #include <px4_platform_common/module.h>
 #include <px4_platform_common/module_params.h>
 #include <uORB/Publication.hpp>
@@ -18,54 +20,70 @@ using namespace time_literals;
 
 extern "C" __EXPORT int flktr2l_main(int argc, char *argv[]);
 
-
-class Flktr2L : public ModuleBase<Flktr2L>, public ModuleParams
-{
+class Flktr2L : public ModuleBase<Flktr2L>, public ModuleParams {
 public:
-	Flktr2L();
+  Flktr2L(const char *port, const speed_t speed = B115200);
 
-	virtual ~Flktr2L() = default;
+  virtual ~Flktr2L() = default;
 
-	/** @see ModuleBase */
-	static int task_spawn(int argc, char *argv[]);
+  /** @see ModuleBase */
+  static int task_spawn(int argc, char *argv[]);
 
-	/** @see ModuleBase */
-	static Flktr2L *instantiate(int argc, char *argv[]);
+  /** @see ModuleBase */
+  static Flktr2L *instantiate(int argc, char *argv[]);
 
-	/** @see ModuleBase */
-	static int custom_command(int argc, char *argv[]);
+  /** @see ModuleBase */
+  static int custom_command(int argc, char *argv[]);
 
-	/** @see ModuleBase */
-	static int print_usage(const char *reason = nullptr);
+  /** @see ModuleBase */
+  static int print_usage(const char *reason = nullptr);
 
-	/** @see ModuleBase::run() */
-	void run() override;
+  /** @see ModuleBase::run() */
+  void run() override;
 
-	/** @see ModuleBase::print_status() */
-	int print_status() override;
+  /** @see ModuleBase::print_status() */
+  int print_status() override;
 
 private:
 
-	/**
-	 * Check for parameter changes and update them if needed.
-	 * @param parameter_update_sub uorb subscription to parameter_update
-	 * @param force for a parameter update
-	 */
-	void parameters_update(bool force = false);
-        void adc();
+  /**
+   * Check for parameter changes and update them if needed.
+   * @param parameter_update_sub uorb subscription to parameter_update
+   * @param force for a parameter update
+   */
+  void parameters_update(bool force = false);
+  //void adc();
+  int open_serial_port(const char *port, const speed_t speed);
 
+  DEFINE_PARAMETERS(
+		    (ParamInt<px4::params::SYS_AUTOSTART>) _param_sys_autostart,   /**< example parameter */
+		    (ParamInt<px4::params::SYS_AUTOCONFIG>) _param_sys_autoconfig  /**< another parameter */
+		    )
 
-	DEFINE_PARAMETERS(
-		(ParamInt<px4::params::SYS_AUTOSTART>) _param_sys_autostart,   /**< example parameter */
-		(ParamInt<px4::params::SYS_AUTOCONFIG>) _param_sys_autoconfig  /**< another parameter */
-	)
+  // Subscriptions
+  uORB::SubscriptionInterval _parameter_update_sub{ORB_ID(parameter_update), 1_s};
+  uORB::Publication<vehicle_command_s> _vehicle_command_pub{ORB_ID(vehicle_command)};
+  uORB::Publication<tool_status_s> _tool_status_pub{ORB_ID(tool_status)};
 
-	// Subscriptions
-	uORB::SubscriptionInterval _parameter_update_sub{ORB_ID(parameter_update), 1_s};
-        uORB::Publication<vehicle_command_s> _vehicle_command_pub{ORB_ID(vehicle_command)};
-        uORB::Publication<tool_status_s> _tool_status_pub{ORB_ID(tool_status)};
+  tool_status_s _msg = {};
 
-        tool_status_s _msg = {};
+  bool _failsafe = false;
 
-        bool _failsafe = false;
+  char _port[20] {};
+  speed_t _speed {};
+  int _file_descriptor{-1};
+
+  const uint16_t sReferenceHeader[2] = {0xFFe5, 0xFFe5};
+  static const int sBufferSize = 40;
+
+  //This must correspond 1:1 to the Package structure on the Teensy side!
+  struct Package {
+    uint16_t _header[2];
+    uint8_t _size = sizeof(Flktr2L::Package);
+    uint32_t _scale = 0;
+  } _package;
+
+  void _shiftAndAdd(uint8_t oneByte); //shift by one byte and add one byte
+  uint8_t _buffer[sBufferSize];
+  hrt_abstime _timestamp_last_sample {};
 };
