9dce6360f204f84e99ed6e66aeebbe709dce9a1c
Henrik Battke <henrik.battke@yahoo.de>
2024-05-25 20:45:07 +0200

    small change, default distance in rangefinder

--- src/modules/tooldrv/tooldrv.cpp
+++ src/modules/tooldrv/tooldrv.cpp
@@ -481,7 +481,7 @@ void ToolDrv::run() {
 		_px4_rangefinder_downward.update(hrt_absolute_time(), _teensy2px4._downward_dist);
 	      }
 	      else {
-		_px4_rangefinder_downward.update(hrt_absolute_time(), sRangFinderMaxDistance, 0);
+		_px4_rangefinder_downward.update(hrt_absolute_time(), sRangFinderDefMaxDistance, 0);
 	      }
 	    }
 	    if(_teensy2px4._mod & PckgTeensy2PX4::FORWARD) {
@@ -489,7 +489,7 @@ void ToolDrv::run() {
 		_px4_rangefinder_forward.update(hrt_absolute_time(), _teensy2px4._forward_dist);
 	      }
 	      else {
-		_px4_rangefinder_forward.update(hrt_absolute_time(), sRangFinderMaxDistance, 0);
+		_px4_rangefinder_forward.update(hrt_absolute_time(), sRangFinderDefMaxDistance, 0);
 	      }
 	    }
 	    /*
